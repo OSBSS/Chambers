290cd3db81751b00160e134421a17e979332fdd4
OSBSS <timemachine@server.local>
2016-03-09 23:28:00 -0600

    Fix script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 // OSBSS Copyright 2016
 
+// AJAX reload function
+function auto_load() {
+	$('body').load('index.php').fadeIn("slow");
+}
+setInterval(auto_load,10000); // refreshing after every 5 seconds
+
 $(document).ready(function(){
  	auto_load(); //Call auto_load() function when DOM is Ready
 });
-
-setInterval(auto_load,10000); // refreshing after every 5 seconds
-
-// AJAX reload function
-function auto_load() {
-	$('body').load(index.php).fadeIn("slow");
-}
