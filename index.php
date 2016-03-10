079e1d8d7ccbbfc7de608afaa07b30517af20630
OSBSS <timemachine@server.local>
2016-03-09 20:56:20 -0600

    Fix table name type.
    Looks like this is it.

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
 		echo '<thead><tr><th>Data Point #</th><th>Time</th><th>Temperature</th><th>CO<sub>2</sub></th><th>Relative Humidity</th><th>Light Intensity</th><th>Surface Temperature</th></tr></thead>';
 		//$table = 'chambers' . $x . '';
 		// SQL query to get all data 
-		$sql = "SELECT * FROM chambers$x ORDER BY id DESC LIMIT 1";
+		$sql = "SELECT * FROM chamber$x ORDER BY id DESC LIMIT 1";
 		echo $sql;
 		$result = mysql_query($sql) or die(mysql_error());
 		$row = mysql_fetch_array($result);
