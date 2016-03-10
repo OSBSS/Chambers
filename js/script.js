fc41125939b3c7a6366bf61940ad2132cc17698d
OSBSS <timemachine@server.local>
2016-03-09 22:59:50 -0600

    Fix AJAX reload data

--- js/script.js
+++ js/script.js
@@ -3,6 +3,6 @@
 // AJAX reload function
 var auto_refresh = setInterval(
 	function() {
-		$('#data').load('../data.php').fadeIn("slow");
+		$('#data').load('data.php').fadeIn("slow");
     }, 5000); 
 // refreshing after every 5 seconds
