4a745e9522e1a1a15ab2c14e641dd76844a3ded5
OSBSS <timemachine@server.local>
2016-03-09 23:18:03 -0600

    Revert changes to data.php

--- data.php
+++ data.php
@@ -1,14 +1,24 @@
 <?php
-echo '<div class="col-md-4">';
-echo '<h4 style="text-align:center; margin-bottom:-15px;">Chamber '. $x . '</h3>';
-echo '<br />';
-// Table start
-echo '<table cellpadding="0" cellspacing="0" class="db-table">';
-echo '<thead><tr><th>Data Point #</th><th>Time</th><th>Temperature</th><th>CO<sub>2</sub></th><th>Relative Humidity</th><th>Light Intensity</th><th>Surface Temperature</th></tr></thead>';
-// Display data
-echo "<tbody><tr><td>" . $row['id'] . "</td><td> " . $row['timestamp'] . "</td><td> " . $row['temp'] . "&deg;C</td><td> " . $row['co2'] . " ppm</td><td> " . $row['rh'] . "%</td><td> " . $row['lux'] . " lux</td><td> " . $row['stemp'] . "&deg;C</td></tr></tbody>"; 
-echo '</table>';
-// Table end
-echo '<div style="text-align: center"><a href="export.php?table=chamber'.$x.'">Export</a></div>';
-echo '</div>';
-?>
+// Start Loop
+    for($x = 1; $x <= $chambers; $x++) {   			
+		// Generate a grid to display table and its data
+		echo '<div class="col-md-4">';
+		echo '<h4 style="text-align:center; margin-bottom:-15px;">Chamber '. $x . '</h3>';
+		echo '<br />';
+		// Table start
+		echo '<table cellpadding="0" cellspacing="0" class="db-table">';
+		echo '<thead><tr><th>Data Point #</th><th>Time</th><th>Temperature</th><th>CO<sub>2</sub></th><th>Relative Humidity</th><th>Light Intensity</th><th>Surface Temperature</th></tr></thead>';
+		//$table = 'chambers' . $x . '';
+		// SQL query to get all data 
+		$sql = "SELECT * FROM chamber$x ORDER BY id DESC LIMIT 1";
+		// echo $sql; Debug test to see if query is correct
+		$result = mysql_query($sql) or die(mysql_error());
+		$row = mysql_fetch_array($result);
+        // Display data
+        echo "<tbody><tr><td>" . $row['id'] . "</td><td> " . $row['timestamp'] . "</td><td> " . $row['temp'] . "&deg;C</td><td> " . $row['co2'] . " ppm</td><td> " . $row['rh'] . "%</td><td> " . $row['lux'] . " lux</td><td> " . $row['stemp'] . "&deg;C</td></tr></tbody>"; 
+		echo '</table>';
+		// Table end
+		echo '<div style="text-align: center"><a href="export.php?table=chamber'.$x.'">Export</a></div>';
+		echo '</div>';
+	}
+// End Loop
