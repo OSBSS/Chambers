7b5e169330cc2b0f78542684cf84787ed73c5d69
OSBSS <timemachine@server.local>
2016-03-09 23:36:21 -0600

    Increase refresh time

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@
 function auto_load() {
 	$('#data').load('data.php').fadeIn("slow");
 }
-setInterval(auto_load,10000); // refreshing after every 5 seconds
+setInterval(auto_load,15000); // refreshing after every 15 seconds
 
 $(document).ready(function(){
- 	auto_load(); //Call auto_load() function when DOM is Ready
+ 	auto_load(); //Call auto_load() function when DOM is Read
 });
