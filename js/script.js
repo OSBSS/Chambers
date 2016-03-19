ddd8035f009c7d09a0059f5309a9db358c42c105
OSBSS <timemachine@server.local>
2016-03-19 04:52:05 -0500

    Countdown bug fix

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 // Initializing ... 
 $(document).ready(function(){
- 	countdown();
+	countdown();
  	auto_load(); //Call auto_load() function when DOM is Read
 });
 
@@ -17,6 +17,7 @@ setInterval(auto_load,15000); // refreshing after every 15 seconds
 // Reload counter
 
 function countdown() {
+	var i = document.getElementById('counter');
     i.innerHTML = parseInt(i.innerHTML)-1;
 }
 setInterval(function(){ countdown(); },1000);
