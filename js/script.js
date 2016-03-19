62048ca0dae3c422daa66cf07abf85e27be3582a
OSBSS <timemachine@server.local>
2016-03-19 04:46:02 -0500

    Add countdown timer

--- js/script.js
+++ js/script.js
@@ -11,3 +11,11 @@ $(document).ready(function(){
 });
 
 /*************************************************************/
+
+// Reload Counter
+
+function countdown() {
+    var i = document.getElementById('counter');
+    i.innerHTML = parseInt(i.innerHTML)-1;
+}
+setInterval(function(){ countdown(); },1000);
