3d9fc89a0e63cd683fee1a97108b899b53e5f695
OSBSS <timemachine@server.local>
2016-03-19 04:04:04 -0500

    Separate truncate action

--- index.php
+++ index.php
@@ -41,29 +41,6 @@ if ($_POST['txtUsername'] != $username || $_POST['txtPassword'] != $password) { 
 
 <?php } ?> 
 
-<?php
-	// Truncate tables action
-	if(isset($_GET['action'])) {
-		switch($_GET['action']) {
-			case 'truncate':
-					mysql_query("TRUNCATE TABLE node1;");
-					mysql_query("ALTER TABLE node1 AUTO_INCREMENT = 1");
-					mysql_query("TRUNCATE TABLE node2;");
-					mysql_query("ALTER TABLE node2 AUTO_INCREMENT = 1");
-					mysql_query("TRUNCATE TABLE node3;");
-					mysql_query("ALTER TABLE node3 AUTO_INCREMENT = 1");
-					mysql_query("TRUNCATE TABLE node4;");
-					mysql_query("ALTER TABLE node4 AUTO_INCREMENT = 1");
-					mysql_query("TRUNCATE TABLE node5;");
-					mysql_query("ALTER TABLE node5 AUTO_INCREMENT = 1");
-					mysql_query("TRUNCATE TABLE node6;");
-					mysql_query("ALTER TABLE node6 AUTO_INCREMENT = 1");
-					header("Location: " . $_SERVER['REQUEST_URI']);
-					break;
-		}
-	}
-?>
-
 <p style="clear:both;"></p>
 <br />
 <footer style="text-align:center;">Copyright &copy; 2016 OSBSS - BERG Lab</footer>
