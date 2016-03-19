783ee6a7bcebc3bb0bde61c5ac58ec93c32cef9d
OSBSS <timemachine@server.local>
2016-03-18 20:53:33 -0500

    Minor edits

--- index.php
+++ index.php
@@ -24,9 +24,9 @@ if ($_POST['txtUsername'] != $username || $_POST['txtPassword'] != $password) { 
 <div style="text-align:center;">
 <h1>OSBSS Login</h1> 
 <form name="form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>"> 
-    <p><label for="txtUsername">Username:</label> 
+    <p><label for="txtUsername">Username</label> 
     <br /><input type="text" title="Enter your username" name="txtUsername" /></p> 
-    <p><label for="txtpassword">Password:</label> 
+    <p><label for="txtpassword">Password</label> 
     <br /><input type="password" title="Enter your password" name="txtPassword" /></p> 
     <p><input type="submit" name="Submit" value="Login" /></p> 
 </form> 
