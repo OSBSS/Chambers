fc5098a5ff083e4de537e3cadc6b7ee22b31a9ff
OSBSS <timemachine@server.local>
2016-03-19 23:10:54 -0500

    Bug fix for DOM load

--- log.php
+++ log.php
@@ -4,12 +4,9 @@
 <title>Chambers Project - Logs</title>
 <script>
 $(document).ready(function(){
- 	auto_load(); //Call auto_load() function when DOM is Read
-});
-function auto_load() {
-	$('#log').load('includes/datalogs.php').fadeIn("slow");
+	$("#log").load("includes/datalogs.php").fadeIn("slow");});
+	setInterval(auto_load,2000); // refreshing after every 1 second
 }
-setInterval(auto_load,1000); // refreshing after every 1 second
 </script>
 </head>
 <body>
