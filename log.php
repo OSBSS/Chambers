bfc257bfbf321124e28e2586f93e15835f9aa1b1
OSBSS <timemachine@server.local>
2016-03-19 22:59:28 -0500

    Output already sent error fix

--- log.php
+++ log.php
@@ -1,7 +1,5 @@
 <!DOCTYPE HTML>
-<!-- OSBSS Copyright 2016 -->
 <html>
-
 <head>
 <title>Chambers Project - Logs</title>
 </head>
@@ -9,7 +7,6 @@
 <body>
 <a href="log.php?action=clear">Clear</a>
 <br />
-
 <?php
     $myfilename = "log.txt";
     if(file_exists($myfilename)){
@@ -20,12 +17,11 @@
 		switch($_GET['action']) {
 			case 'clear':
 					file_put_contents("log.txt", "");
-					header("Location: http://data.osbss.com/chambers/logs.php");
+					header("Location: log.php");
 					echo "Log cleared!";
 					break;
 		}
 	}
 ?>
-
-</html>
 </body>
+</html>
