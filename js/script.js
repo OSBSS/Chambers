49c182bdb8c8a6895dde4590a77e1f34bd935642
OSBSS <timemachine@server.local>
2016-03-21 00:40:21 -0500

    Modify counter

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 // OSBSS Copyright 2016
 
 // Initializing ... 
-$(document).ready(function(){
- 	
- 	auto_load(); //Call auto_load() function when DOM is Read
+$(document).ready(function(){	
+ 	//auto_load(); //Call auto_load() function when DOM is Read
  	countdown();
+}); 	
+
+function countdown(remaining) {
+    if(remaining === 0)
+        auto_load();
+    document.getElementById('counter').innerHTML = remaining;
+    setTimeout(function(){ countdown(remaining - 1); }, 1000);
+}(5); 	
+ 	
  	
 // AJAX reload
 function auto_load() {
@@ -12,13 +20,18 @@ function auto_load() {
 }
 setInterval(auto_load,5000); // refreshing after every 5 seconds
 
-// Reload counter
+/* Reload counter
 function countdown() {
 	var i = document.getElementById('counter');
     i.innerHTML = parseInt(i.innerHTML)-1;
 }
 setInterval(countdown,1000);
 
-});
+*/
+
+
+
+
+
 
 
