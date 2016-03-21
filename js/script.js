dc8b5627fa35267c06dad3f6bf7593dadfbc0ec6
OSBSS <timemachine@server.local>
2016-03-21 00:30:38 -0500

    Modify script.js

--- js/script.js
+++ js/script.js
@@ -4,18 +4,23 @@
 $(document).ready(function(){
  	auto_load(); //Call auto_load() function when DOM is Read
  	countdown();
-});
+ 	
+ 	
+// Reload counter
+function countdown() {
+	var i = document.getElementById('counter');
+    i.innerHTML = parseInt(i.innerHTML)-1;
+}
+setInterval(countdown,1000);
 
 // AJAX reload
 function auto_load() {
 	$('#data').load('data.php').fadeIn("slow");
 }
 setInterval(auto_load,5000); // refreshing after every 5 seconds
+});
 
-// Reload counter
 
-function countdown() {
-	var i = document.getElementById('counter');
-    i.innerHTML = parseInt(i.innerHTML)-1;
-}
-setInterval(function(){ countdown(); },1000);
+
+
+
