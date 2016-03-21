e8d06590f887526ec05254bbd16596287b2d8048
OSBSS <timemachine@server.local>
2016-03-21 00:55:04 -0500

    Merge ajax reload with counter

--- js/script.js
+++ js/script.js
@@ -2,12 +2,10 @@
 
 //Initializing ... 
 $(document).ready(function(){	
- 	//auto_load(); //Call auto_load() function when DOM is Read
  	countdown();
 }); 	
 
 var countDown=5;
-
 function countdown() {
         setInterval(function () {
             if (countDown == 0) {
@@ -18,13 +16,5 @@ function countdown() {
             document.getElementById('counter').innerHTML = countDown;
             return countDown;
         }, 1000);
-    }
-
-/* AJAX reload
-function auto_load() {
-	$('#data').load('data.php').fadeIn("slow");
 }
-setInterval(auto_load,5000); // refreshing after every 5 seconds
-
-*/
 
